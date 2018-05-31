9cf4a41af324ef646d0fdf96de2b11b55ae0a6c1
hyuk <misterhyuk@naver.com>
2018-05-31 13:42:02 +0900

    잘게 나누어서 온오프 해결 아직 :form열릴때/닫힐때 설정은 안했음

---
+++ csharp - 온오프 해결/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnInit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(134, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(834, 620);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 419);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(27, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(27, 224);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(27, 323);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(24, 15);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(77, 35);
+            this.btnInit.TabIndex = 6;
+            this.btnInit.Text = "Init";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 656);
+            this.Controls.Add(this.btnInit);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnInit;
+    }
+}
+
---
+++ csharp - 온오프 해결/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+using Sentech.GenApiDotNET;
+using Sentech.StApiDotNET;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace csharp
+{
+
+    public partial class Form1 : Form
+    {
+        //Thread camera;
+        IStImage stImage;
+        Bitmap map;
+        bool stop = false;
+        bool view = false;
+        CStApiAutoInit api;
+        CStSystem system;
+        CStDevice device;
+        CStDataStream dataStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void OnCallback(IStCallbackParamBase paramBase, object[] param)
+        {
+            Console.WriteLine("8");
+
+            if (paramBase.CallbackType == eStCallbackType.TL_DataStreamNewBuffer)
+                Console.WriteLine("9");
+            {
+                IStCallbackParamGenTLEventNewBuffer callbackParam = paramBase as IStCallbackParamGenTLEventNewBuffer;
+                Console.WriteLine("10");
+
+                if (callbackParam != null)
+                Console.WriteLine("11");
+                {
+                    IStDataStream dataStream = callbackParam.GetIStDataStream();
+                    Console.WriteLine("12");
+
+                    using (CStStreamBuffer streamBuffer = dataStream.RetrieveBuffer(0))
+
+                    {
+                        Console.WriteLine("13");
+
+                        if (streamBuffer.GetIStStreamBufferInfo().IsImagePresent)
+                        {
+                            Console.WriteLine("14");
+
+                            stImage = streamBuffer.GetIStImage();
+                            Console.WriteLine("15");
+
+                            byte[] imageData = stImage.GetByteArray();
+                            map = new Bitmap((int)stImage.ImageWidth, (int)stImage.ImageHeight,
+                           (int)stImage.ImageWidth, PixelFormat.Format24bppRgb, Marshal.UnsafeAddrOfPinnedArrayElement(stImage.GetByteArray(), 0));
+                            Console.WriteLine("16");
+                            //if(view==true) pictureBox1.Image = map;
+                            Console.WriteLine("17");//인보크 딜리게이트
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public void start()
+        {
+            {
+                dataStream.StartAcquisition();
+                Console.WriteLine("3");
+
+                device.AcquisitionStart();
+                Console.WriteLine("4");
+            }
+        }
+
+        public void stop1()
+        {
+            Console.WriteLine("5");
+
+            device.AcquisitionStop();
+            Console.WriteLine("6");
+
+            dataStream.StopAcquisition();
+            Console.WriteLine("7");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //camera = new Thread(new ThreadStart(start));
+            //camera.Start();
+            //pictureBox1.Image = map;
+            start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stop = true;
+            stop1();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            view = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            view = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = map;
+        }
+
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            api = new CStApiAutoInit();
+            system = new CStSystem(eStSystemVendor.Sentech);
+            device = system.CreateFirstStDevice();
+            dataStream = device.CreateStDataStream(0);
+
+            Console.WriteLine("1");
+            dataStream.RegisterCallbackMethod(OnCallback);
+            Console.WriteLine("2");
+        }
+    }
+}
